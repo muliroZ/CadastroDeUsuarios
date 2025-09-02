5a0757543a83831c96db95738b3c4492e4db2648
muliroZ <murilomega100@gmail.com>
2025-09-02 17:47:58 -0300

    Criação do service e do repository

--- src/main/java/com/muriloscorp/cadastrodeusuarios/repository/UserRepository.java
+++ src/main/java/com/muriloscorp/cadastrodeusuarios/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.muriloscorp.cadastrodeusuarios.repository;
 
-public interface UserRepository {
+import com.muriloscorp.cadastrodeusuarios.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByNome(String nome);
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/muriloscorp/cadastrodeusuarios/service/UserService.java
+++ src/main/java/com/muriloscorp/cadastrodeusuarios/service/UserService.java
@@ -1,4 +1,30 @@
 package com.muriloscorp.cadastrodeusuarios.service;
 
+import com.muriloscorp.cadastrodeusuarios.model.User;
+import com.muriloscorp.cadastrodeusuarios.repository.UserRepository;
+import jakarta.annotation.Nonnull;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public User criarUser(@Nonnull User user) {
+        if (user.getNome().isEmpty() || user.getEmail().isEmpty()) {
+            throw new IllegalArgumentException("Nome e email são obrigatórios.");
+        }
+        return userRepository.save(user);
+    }
+
+    public Optional<User> buscarPorNome(String nome) {
+        return userRepository.findByNome(nome);
+    }
+
+    public Optional<User> buscarPorEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
 }
