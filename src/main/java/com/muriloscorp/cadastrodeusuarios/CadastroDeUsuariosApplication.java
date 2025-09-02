18511164e52f264f4336123c67c42f3e27f1e20c
muliroZ <murilomega100@gmail.com>
2025-09-02 17:25:44 -0300

    Projeto iniciado

---
+++ src/main/java/com/muriloscorp/cadastrodeusuarios/CadastroDeUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.muriloscorp.cadastrodeusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeUsuariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroDeUsuariosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muriloscorp/cadastrodeusuarios/CadastroDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muriloscorp.cadastrodeusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeUsuariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
