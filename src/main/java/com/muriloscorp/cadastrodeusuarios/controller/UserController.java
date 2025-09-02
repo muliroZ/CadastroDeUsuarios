c585a474c7b146db8d40fd1e03e57e55fba96621
muliroZ <murilomega100@gmail.com>
2025-09-02 19:34:38 -0300

    Criação do UserController

--- src/main/java/com/muriloscorp/cadastrodeusuarios/controller/UserController.java
+++ src/main/java/com/muriloscorp/cadastrodeusuarios/controller/UserController.java
@@ -1,4 +1,43 @@
 package com.muriloscorp.cadastrodeusuarios.controller;
 
+import com.muriloscorp.cadastrodeusuarios.model.User;
+import com.muriloscorp.cadastrodeusuarios.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.buscarTodos();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/nome/{name}")
+    public ResponseEntity<User> getUserByName(@PathVariable String name) {
+        return userService.buscarPorNome(name)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        return userService.buscarPorEmail(email)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User user) {
+        User newUser = userService.criarUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
 }
