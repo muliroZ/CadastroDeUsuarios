cfc802dea5a1845f111cf3c19b1f92cef4625d57
muliroZ <murilomega100@gmail.com>
2025-09-02 18:54:46 -0300

    Alteração no nome da tabela User (conflito)

--- src/main/java/com/muriloscorp/cadastrodeusuarios/model/User.java
+++ src/main/java/com/muriloscorp/cadastrodeusuarios/model/User.java
@@ -1,15 +1,13 @@
 package com.muriloscorp.cadastrodeusuarios.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "app_users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
